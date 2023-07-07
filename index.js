b433d3ca4f080d189cd7ac15c342e8fa239f0020
Petr Haton <petr.haton@unicov-net.com>
2023-07-07 17:37:15 +0200

    Pridani input promenne

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const github = require('@actions/github');
 try {
   // Get Input
   const workingDir = core.getInput('working-directory');
-  console.log("Working directory: " + workingDir);
+  console.log("Working directory:", workingDir);
+  const envFile = core.getInput('env-file-name');
+  console.log("File from environments:", envFile);
   const tmpDir = core.getInput('tmp-directory');
-  console.log("Temporary directory k8s manifests" + tmpDir);
+  console.log("Temporary directory k8s manifests:", tmpDir);
+
+  console.log("Current directory:", __dirname);
 
   // Set Output
   const time = (new Date()).toTimeString();
