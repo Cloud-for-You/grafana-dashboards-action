5e6ed3fabd77dd93f5b84bf7e42c0e037f990c63
Petr Haton <petr.haton@unicov-net.com>
2023-07-07 17:57:16 +0200

    Nastaveni working directory pres promennout

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
   // Get Input
   const workingDir = core.getInput('working-directory');
+  fs.readdirSync(workingDir).forEach(file => {
+    console.log(file);
+  });
+    
   console.log("Working directory:", workingDir);
+
+  
   const envFile = core.getInput('env-file-name');
   console.log("File from environments:", envFile);
   const tmpDir = core.getInput('tmp-directory');
@@ -13,8 +20,6 @@ try {
   // Set Output
   const time = (new Date()).toTimeString();
   core.setOutput('time', time);
-
-
 } catch (error) {
   core.setFailed(error.message)  
 }
