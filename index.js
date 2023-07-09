d755e0d742a9574fcbc13d6f9de76a5bf9ff76fd
Petr Haton <petr.haton@unicov-net.com>
2023-07-09 19:18:04 +0200

    Prejmenovani funkce

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ try {
       // spustime vykonny kod pro renderovani
       // Scriptu, predlozime vyhledany ENV soubor, ktery si modul rozparsuje a nasledne vygeneruje
       // k8s manifesty a vlozi do nej dashboard z uvedeneho zdroje
-      manifests.init(configFile);
+      manifests.render(configFile);
     }
   } else {
     console.log('Files is not found.');
