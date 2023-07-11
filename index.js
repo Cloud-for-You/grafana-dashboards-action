e3370e54d741d1995d3921de85fcf487ade8e1a6
Petr Haton <petr.haton@unicov-net.com>
2023-07-11 10:44:29 +0200

    Zapnuti patchovani objektu

--- index.js
+++ index.js
@@ -26,16 +26,17 @@ function getConfigFiles(directory, fileName, result = []) {
 
 try {
   // testovaci vstupy
-  /*
+  
   const workingDir = '../grafana-dashboards';
   const configFile = 'config.yaml';
   const tmpDir = '/tmp/k8s_manifests';
-  */
+  
   // Ziskani vstupu z github actions
+  /*
   const workingDir = core.getInput('working-directory');
   const configFile = core.getInput('config-file-name');
   const tmpDir = core.getInput('tmp-directory');
-
+  */
   
 
   // Pripravime si tmp directory pokud neexistuje
